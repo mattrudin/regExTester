6ca934122b5298d9bae50a85b2e45631427b86db
Matthias Rudin <matthias.rudin@live.com>
2019-01-15 05:31:06 +0100

    Added isValidPhoneNumber and tests

--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 export const isValidHex = text => /(^#[A-Fa-f0-9]{6}$)|(^#[A-Fa-f0-9]{3}$)/ig.test(text);
 export const isValidEmail = email => /^[^@]+@[^@]+\.[a-z]+$/ig.test(email);
+export const isValidPhoneNumber = number => /^\D*\d{3}\D*\d{3}\D*\d{4}\D*$/ig.test(number);
--- main.test.js
+++ main.test.js
@@ -1,4 +1,4 @@
-import { isValidHex, isValidEmail } from './main';
+import { isValidHex, isValidEmail, isValidPhoneNumber } from './main';
 
 describe('Test suite for Hex test function', () => {
     test('#FF4569 to be true', () => {
@@ -28,4 +28,22 @@ describe('Test suite for email verification',() => {
     test('abc@abc.com to be true', () => {
         expect(isValidEmail('abc@abc.com')).toBeTruthy();
     })
+})
+
+describe('Test suite for phone number verification', () => {
+    test('1234567890 to be true', () => {
+        expect(isValidPhoneNumber(1234567890)).toBeTruthy();
+    });
+    test('(123) 456 7890 to be true', () => {
+        expect(isValidPhoneNumber('(123) 456 7890')).toBeTruthy();
+    });
+    test('(123) 456-7890 to be true', () => {
+        expect(isValidPhoneNumber('(123) 456-7890')).toBeTruthy();
+    });
+    test('12345678901 to be false', () => {
+        expect(isValidPhoneNumber(12345678901)).toBeFalsy();
+    });
+    test('12345678 to be false', () => {
+        expect(isValidPhoneNumber(12345678)).toBeFalsy();
+    });
 })
